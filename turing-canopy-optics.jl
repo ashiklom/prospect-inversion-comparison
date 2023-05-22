a9bbd4b3a541a3d99337f77d3ff082dd9bf021c1
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-05-22 16:32:15 -0400

    More exploration of PROSPECT inversion

--- turing-canopy-optics.jl
+++ turing-canopy-optics.jl
@@ -1,12 +1,8 @@
 using CanopyOptics
 using Unitful
 using Turing
-using Optim
 using Distributions
 using ForwardDiff
-using Plots
-using StatsPlots
-using FillArrays
 using LinearAlgebra
 
 # See Turing documentation: https://turing.ml/dev/docs/using-turing/guide
@@ -15,37 +11,16 @@ using LinearAlgebra
 const opti_c = createLeafOpticalStruct((400.0:1:2500) * u"nm")
 const leaf_c = LeafProspectProProperties{Float64}(N=1.4, Ccab = 40, Cw = 0.01, Cm = 0.01)
 const _, obs = prospect(leaf_c, opti_c)
-plot(obs)
+# using Plots
+# plot(obs)
 
-# Try writing prospect4 as a separate project
 function prospect4(N::T, Cab::T, Cw::T, Cm::T) where {T}
     leaf = LeafProspectProProperties{T}(N=N, Ccab=Cab, Cw=Cw, Cm=Cm)
     _,R = prospect(leaf, opti_c)
     return R
 end
 
-@model function fitprospect(obs, ::Type{T} = Float64) where {T}
-    # Priors
-    N ~ Uniform(1.1, 3)
-    Cab ~ Uniform(10, 100)
-    Cw ~ Uniform(0.001, 0.02)
-    Cm ~ Uniform(0.001, 0.02)
-    resid ~ InverseGamma(1, 0.2)
-    mod = prospect4(N, Cab, Cw, Cm)
-    for i in eachindex(obs)
-        obs[i] ~ Normal(mod[i], resid)
-    end
-end
-
-# Works, but somewhat slow. 5000 iterations took 659.27 seconds (~11 minutes).
-# 500 steps takes ~35 seconds.
-chain_f = sample(fitprospect(obs), NUTS(), 500)
-
-# Both of these work. MLE produces an estimate much closer to the truth.
-tmap = optimize(fitprospect(obs), MAP())
-tmle = optimize(fitprospect(obs), MLE())
-
-@model function fitprospect_mv(obs, ::Type{T} = Float64) where {T}
+@model function fitprospect(obs)
     # Priors
     N ~ Uniform(1.1, 3)
     Cab ~ Uniform(10, 100)
@@ -56,10 +31,58 @@ tmle = optimize(fitprospect(obs), MLE())
     obs ~ MvNormal(mod, resid * I)
 end
 
-# This works a tiny bit faster than univariate version, but is still slow
-# 500 steps takes 39.33 seconds
-chain_mv = sample(fitprospect_mv(obs), NUTS(), 500)
-plot(chain_mv)
+function my_fit_prospect(n)
+    sample(fitprospect(obs), NUTS(), n)
+end
+
+my_fit_prospect(10) # Precompile
+my_fit_prospect(500) # 13-17 seconds
+my_fit_prospect(2500) # 58 seconds
+
+using Profile, PProf
+@profile my_fit_prospect(10)
+@profile my_fit_prospect(500)
+
+function tracker_fit_prospect(n)
+    sample(fitprospect(obs), NUTS{Turing.TrackerAD}(), n)
+end
+tracker_fit_prospect(10)  # Precompile
+tracker_fit_prospect(500) # 52.49 seconds
+
+using ReverseDiff
+function reverse_fit_prospect(n)
+    Turing.setadbackend(:reversediff)
+    Turing.setrdcache(true)
+    sample(fitprospect(obs), NUTS(), n)
+end
+reverse_fit_prospect(10)  # Precompile
+reverse_fit_prospect(500) # 
+
+using Zygote
+function zygote_fit_prospect(n)
+    Turing.setadbackend(:zygote)
+    Turing.setrdcache(false)
+    sample(fitprospect(obs), NUTS{}(), n)
+end
+
+zygote_fit_prospect(10)
+zygote_fit_prospect(500)
+
+
+pprof()
+
+@profview my_fit_prospect(10)
+@profview my_fit_prospect(500)
+
+
+# Works, but somewhat slow. 5000 iterations took 659.27 seconds (~11 minutes).
+# 500 steps takes ~35 seconds.
+chain_f = sample(fitprospect(obs), NUTS(), 500)
+
+# Both of these work. MLE produces an estimate much closer to the truth.
+using Optim
+tmap = optimize(fitprospect(obs), MAP())
+tmle = optimize(fitprospect(obs), MLE())
 
 using DynamicHMC
 # Startup takes a while...but then, sampling takes ~37 seconds 
@@ -70,7 +93,7 @@ plot(chain_dhmc)
 rΦ = Matrix(Diagonal(fill(0.2, length(obs))))
 riw = InverseWishart(length(obs), rΦ)
 
-@model fitprospect_mv2(obs) = begin
+@model function fitprospect_mv(obs, ::Type{T} = Float64) where {T}
     # Priors
     N ~ Uniform(1.1, 3)
     Cab ~ Uniform(10, 100)
