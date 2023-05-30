1df6731d95e889a8c964a6ebbad64ad99be2a5d8
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-05-30 10:15:22 -0400

    Working DynamicHMC example

--- dhmc-canopt.jl
+++ dhmc-canopt.jl
@@ -5,12 +5,13 @@ using ForwardDiff
 using LinearAlgebra
 using SimpleUnPack
 using Unitful
+using Random
 
 const opti_c = createLeafOpticalStruct((400.0:1:2500) * u"nm")
-function prospect4(N, Cab, Cw, Cm)
-    leaf = LeafProspectProProperties{Float64}(N=N, Ccab=Cab, Cw=Cw, Cm=Cm)
-    T,R = prospect(leaf, opti_c)
-    R
+function prospect4(N::T, Cab::T, Cw::T, Cm::T) where {T}
+    leaf = LeafProspectProProperties{T}(N=N, Ccab=Cab, Cw=Cw, Cm=Cm)
+    _,R = prospect(leaf, opti_c)
+    return R
 end
 
 struct Model{T}
@@ -20,9 +21,9 @@ end
 function (m::Model)(θ)
     @unpack N, Cab, Cw, Cm, resid = θ
     priors = [
-        Uniform(1.0, 3.0),
-        Uniform(0, 150),
-        Uniform(0, 0.1),
+        Normal(1.0, 3.0),
+        Normal(40, 20),
+        Normal(0, 0.1),
         Uniform(0, 0.1),
         Uniform(0, 1)
     ]
@@ -30,13 +31,13 @@ function (m::Model)(θ)
     isfinite(log_prior) || return -Inf
     mod = prospect4(N, Cab, Cw, Cm)
     dist = MvNormal(mod, resid * I)
-    log_likelihood = sum(logpdf(dist, m.observations))
+    log_likelihood = logpdf(dist, m.observations)
     log_prior + log_likelihood
 end
 
-const obs = prospect4(1.4, 40, 0.01, 0.01)
+const obs = prospect4(1.4, 40.0, 0.01, 0.01)
 m = Model(obs)
-ttest = (N=1.4, Cab=40, Cw=0.01, Cm=0.01, resid=0.1)
+ttest = (N=1.4, Cab=40.0, Cw=0.01, Cm=0.01, resid=0.1)
 # Check...
 m(ttest)
 
@@ -44,8 +45,25 @@ m(ttest)
 using LogDensityProblems, TransformVariables, TransformedLogDensities
 using LogDensityProblemsAD
 
-l = TransformedLogDensity(as((N = asℝ₊, Cab = asℝ₊, Cw = asℝ₊, Cm = asℝ₊, resid = asℝ₊)), m)
-LogDensityProblems.logdensity(m, ttest)
+trans = as((
+    N = as(Real, 1.0, 3.0),
+    Cab = as(Real, 0.0, 100.0),
+    Cw = as(Real, 0.0, 0.1),
+    Cm = as(Real, 0.0, 0.1),
+    resid = as(Real, 0.0, 1.0)
+))
+l = TransformedLogDensity(trans, m)
 ∇l = ADgradient(:ForwardDiff, l)
+
+# Test out 
+using BenchmarkTools
+x = randn(LogDensityProblems.dimension(∇l))
+@benchmark LogDensityProblems.logdensity_and_gradient($∇l, $x)
+
+# Sample
+r = mcmc_with_warmup(Random.default_rng(), ∇l, 100)
+
+results = map(_ -> mcmc_with_warmup(Random.default_rng(), ∇l, 1000), 1:3)
+
 LogDensityProblems.capabilities(∇l)
 LogDensityProblems.logdensity_and_gradient(∇l, (1.4, 40, 0.01, 0.01, 0.3))
